8b93b0c2d950b6be923be0460751b289c44ede96
Oscar Garza <ogarza98@live.com>
2020-02-08 19:31:31 -0600

    base project commit

--- App.js
+++ App.js
@@ -1,24 +1,6 @@
-import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import CommunityPage from './Pages/CommunityPage';
-import QuestionsPage from './Pages/QuestionsPage';
-import FormPage from './Pages/FormPage';
-import NotificationPage from './Pages/NotificationPage';
-
-const Drawer = createDrawerNavigator();
+import React from 'react';
+import HybridApp from './src/App';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Community">
-        <Drawer.Screen name="Community" component={CommunityPage} />
-        <Drawer.Screen name="FAQ" component={QuestionsPage} />
-        <Drawer.Screen name="Notifications" component={NotificationPage} />
-        <Drawer.Screen name="Forms" component={FormPage} />
-
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+  return <HybridApp />;
 }
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import CommunityPage from './pages/CommunityPage';
+import QuestionsPage from './pages/QuestionsPage';
+import FormPage from './pages/FormPage';
+import NotificationPage from './pages/NotificationPage';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+
+    <NavigationContainer>
+    
+      <Drawer.Navigator initialRouteName="Community">
+        <Drawer.Screen name="Community" component={CommunityPage} />
+        <Drawer.Screen name="FAQ" component={QuestionsPage} />
+        <Drawer.Screen name="Notifications" component={NotificationPage} />
+        <Drawer.Screen name="Forms" component={FormPage} />
+
+      </Drawer.Navigator>
+      
+    </NavigationContainer>
+
+  
+  );
+} 
