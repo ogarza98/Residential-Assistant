9a05cde5750f00217afba5e0a4d63dceddb2e586
Oscar Garza <ogarza98@live.com>
2020-02-09 16:14:01 -0600

    fixed web

---
+++ src/AppWeb.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+class AppWeb extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+       <Text>Residential Assistant Website</Text>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 50
+  }
+});
+
+
+
+export default AppWeb;
