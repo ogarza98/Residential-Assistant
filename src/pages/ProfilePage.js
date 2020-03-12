2dd88ef48c71074909b6463cdacd2ec607fc4bc5
Oscar Garza <ogarza98@live.com>
2020-03-12 01:37:07 -0500

    working on printing from db

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,46 +1,44 @@
 import * as React from 'react';
-import { Button, View, Text, Image } from 'react-native';
 import styles from './components/styles'
 import { Header } from 'react-native-elements';
+import * as firebase from "firebase";
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 
-function ProfilePage({ route, navigation }) {
-    
-    /* 2. Get the param */
-    const { firstName } = route.params;
-    const { lastName } = route.params;
-    const { avatar } = route.params;
 
-    var avatarImg = JSON.stringify(avatar);
 
-    var avatar_string = new String();
+export default class ProfilePage extends React.Component {
 
-    avatar_string = avatarImg.toString().replace(/"/g, "");
-
-    var name = JSON.stringify(firstName) + ' ' + JSON.stringify(lastName);
-
-    var name_string = new String();
-
-    name_string = name.toString().replace(/"/g, "");
-
-    return (
-        <View style={styles.container}>
-         <Header
-        leftComponent={{ icon: 'menu', color: '#fff', onPress: () => navigation.toggleDrawer() }}
-        centerComponent={{ text: 'Forms Page', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'notifications', color: '#fff', onPress: () => navigation.navigate('Notifications') }}
-        />
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={styles.profile_title}>{name_string}</Text>
-        <Image
-          style={{width: 100, height: 100, borderRadius: 400/ 2}}
-          source={{uri: avatar_string}}
-        />
-        
-      </View>
-      </View>
-
-      
-    );
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: false,
+      userData: null
+   }
+   this.searchedText = ""
+   this.localData=null;
   }
+  
+  componentDidMount() {
+    // firebase.database().ref('users/').once('value', function (snapshot) {
+    //     console.log(snapshot.val())
+    // });
+    console.log(firebase.auth().currentUser.uid);
 
-export default ProfilePage;
+    var my_uid = firebase.auth().currentUser.uid;
+
+    firebase.database().ref('users/' + my_uid).once('value', function (snapshot) {
+      console.log(snapshot.val());
+
+    });
+
+}
+
+  render() {
+    return (
+      <View style={styles.login_container}>
+        <ActivityIndicator size="large" />
+        <Text>{this.state.userData}</Text>
+      </View>
+    )
+  }
+}
