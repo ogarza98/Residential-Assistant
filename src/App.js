48feb45ea7d970e1d03a1f5ca547a05d17c58fb8
Erick Akins <erickakins98@gmail.com>
2020-03-24 09:55:29 -0500

    Added webpages and buttons
    User can access webpages for work orders and laundry with buttons for
    control. There are buttons to reload, go back a page, go forward a page
    and return to the initial page. The aesthetic still needs to be updated.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import CommunityPage from './pages/CommunityPage';
 import QuestionsPage from './pages/QuestionsPage';
 import FormPage from './pages/FormPage';
 import NotificationPage from './pages/NotificationPage';
+import LaundryPage from './pages/LaundryPage';
+import WorkOrdersPage from './pages/WorkOrdersPage';
 
 const Drawer = createDrawerNavigator();
 
@@ -19,6 +21,8 @@ export default function App() {
         <Drawer.Screen name="FAQ" component={QuestionsPage} />
         <Drawer.Screen name="Notifications" component={NotificationPage} />
         <Drawer.Screen name="Forms" component={FormPage} />
+        <Drawer.Screen name="Work Orders" component={WorkOrdersPage} />
+        <Drawer.Screen name="Laundry" component={LaundryPage} />
 
       </Drawer.Navigator>
       
